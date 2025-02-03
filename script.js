3d7bee98295512ab299c069905e1df3a6c4bb2af
AboubakarJay <aboubakarjay2@hotmail.com>
2025-02-03 16:49:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let links = document.querySelectorAll(".itemLinks");
+let wrapper = document.querySelector("#wrapper");
+let activeLink = 0;
+
+for (let i = 0; i < links.length; i++) {
+  let link = links[i];
+  link.addEventListener("click", setClickedItem);
+  link.itemID = i;
+}
+
+links[activeLink].classList.add("active");
+
+function setClickedItem(e) {
+  removeActiveLinks();
+  let clickedLink = e.target;
+  activeLink = clickedLink.itemID;
+  changePosition(clickedLink);
+}
+function removeActiveLinks() {
+  for (let i = 0; i < links.length; i++) {
+    links[i].classList.remove("active");
+  }
+}
+function changePosition(link) {
+  let position = link.getAttribute("data-pos");
+  let translateValue = `translate3d(${position}, 0px, 0)`;
+  wrapper.style.transform = translateValue;
+  link.classList.add("active");
+}
